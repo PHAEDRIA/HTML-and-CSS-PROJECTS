3abcccfccafc46ed15ae630d8e7eb31a666f33a5
PHAEDRIA <phaedriathayer@gmail.com>
2022-12-19 23:27:56 -0800

    <git Commit>

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <script src="Basic_Javascript_1"></script>
+    <!DOCTYPE html>
+<html>
+<body>
+    <button onclick="My_First_Funtion()">
+        Click me!
+    </button>
+    <p id="Irish"></p>
+<script>
+    function My_First_Function() {
+        var String = "Kiss me, I'm Irish!";
+        var result = string.fontcolor("green");
+        document.getElementById("Irish").innerHTML =
+        result;
+        
+    }
+</script>
+</body>
+</html>
+</html>
